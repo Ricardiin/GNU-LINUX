e714b525f7f25a61459e16a791415d75c576eaed
Ricardiin <Richard.foulu@gmail.com>
2025-07-04 18:32:37 -0300

    att

---
+++ AULAS VAAMONDE/desafio/richard.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+    <html lang="pt-br">
+        <head>
+            <title>Teste da Linguagem PHP</title>
+            <meta charset="utf-8">
+        </head>
+        <body>
+            <!-- Início do script PHP: ?php -->
+            <!-- Mais informações acesse: https://www.w3schools.com/php/php_syntax.asp -->
+            <?php 
+                // Função ECHO: Imprimir uma ou mais strings na saída padrão
+                // Mais informações acesse: https://www.w3schools.com/php/func_string_echo.asp
+                echo '<h1>Teste da Linguagem HTML (HyperText Markup Language)</h1>';
+                echo 'Autor......: Robson Vaamonde<br>';
+                echo 'Editado por: SEU NOME AQUI<br>';
+                echo 'LinkedIn...: https://www.linkedin.com/in/robson-vaamonde-0b029028/<br>';
+                echo 'Site.......: http://procedimentosemti.com.br/<br>';
+                echo 'Facebook...: https://www.facebook.com/ProcedimentosEmTI<br>';
+                echo 'Facebook...: https://www.facebook.com/BoraParaPratica<br>';
+                echo 'Instagram..: https://www.instagram.com/procedimentoem/<br>';
+                echo 'YouTube....: https://youtube.com/BoraParaPratica<br>'; 
+            // Fechamento do Script PHP
+            ?>
+        </body>
+    </html>
